f6bf7c12f9aae1e32049c8b307ea7fb570ed4744
zvHristov <zv.hristov@gmail.com>
2020-01-02 05:56:52 +0200

    add actions add/delete/update

--- demo-app/src/actions/ActionTypes.ts
+++ demo-app/src/actions/ActionTypes.ts
@@ -1,13 +1,48 @@
-import { IGetCompanySuccessAction,  IGetCompanyProgressAction, IGetCompanyFailAction } from './company/get';
 import {
-    IGetCompanyAddressesSuccessAction, IGetCompanyAddressesProgressAction, IGetCompanyAddressesFailAction
+    IGetCompanySuccessAction,
+    IGetCompanyProgressAction,
+    IGetCompanyFailAction } from './company/get';
+import {
+    IGetCompanyAddressesSuccessAction,
+    IGetCompanyAddressesProgressAction,
+    IGetCompanyAddressesFailAction
 } from './company-addresses/get';
 import {
     IGetEmployeeSuccessAction, IGetEmployeeProgressAction, IGetEmployeeFailAction
 } from './employee/get';
 import {
+ IAddEmployeeSuccessAction,
+    IAddEmployeeProgressAction,
+    IAddEmployeeFailAction
+} from './employee/add';
+import {
+IDeleteEmployeeSuccessAction,
+    IDeleteEmployeeProgressAction,
+    IDeleteEmployeeFailAction
+} from './employee/delete';
+import {
+IUpdateEmployeeSuccessAction,
+    IUpdateEmployeeProgressAction,
+    IUpdateEmployeeFailAction
+} from './employee/update';
+import {
     IGetProjectFailAction, IGetProjectProgressAction, IGetProjectSuccessAction
 } from './project/get';
+import {
+  IAddProjectSuccessAction,
+    IAddProjectProgressAction,
+    IAddProjectFailAction
+} from './project/add';
+import {
+   IUpdateProjectSuccessAction,
+    IUpdateProjectProgressAction,
+    IUpdateProjectFailAction
+} from './project/update';
+import {
+   IDeleteProjectSuccessAction,
+    IDeleteProjectProgressAction,
+    IDeleteProjectFailAction
+} from './project/delete';
 type ActionTypes =
     | IGetCompanySuccessAction
     | IGetCompanyFailAction
@@ -18,7 +53,25 @@ type ActionTypes =
     | IGetProjectFailAction
     | IGetProjectProgressAction
     | IGetProjectSuccessAction
+    | IAddProjectSuccessAction
+    | IAddProjectProgressAction
+    | IAddProjectFailAction
+    | IUpdateProjectSuccessAction
+    | IUpdateProjectProgressAction
+    | IUpdateProjectFailAction
+    | IDeleteProjectSuccessAction
+    | IDeleteProjectProgressAction
+    | IDeleteProjectFailAction
     | IGetEmployeeSuccessAction
     | IGetEmployeeProgressAction
-    | IGetEmployeeFailAction;
+    | IGetEmployeeFailAction
+    | IAddEmployeeSuccessAction
+    | IAddEmployeeProgressAction
+    | IAddEmployeeFailAction
+    | IDeleteEmployeeSuccessAction
+    | IDeleteEmployeeProgressAction
+    | IDeleteEmployeeFailAction
+    | IUpdateEmployeeSuccessAction
+    | IUpdateEmployeeProgressAction
+    | IUpdateEmployeeFailAction;
 export default ActionTypes;
